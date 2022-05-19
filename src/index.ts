0dead33f550dfdf5122dc58b76a06e974e331649
FernandoTDF <104107236+FernandoTDF@users.noreply.github.com>
2022-05-18 21:46:57 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let num: number[] = new Array(7);
+
+let indice: number = 0;
+
+num[0] = 20;
+num[1] = 15;
+num[2] = 8;
+num[3] = 0;
+num[4] = 5;
+num[5] = 19;
+num[6] = 24;
+
+console.log(num); 
+
+while (indice < 7) {
+  console.log ("El número en la posición ", indice, " es ", num[indice]);
+  indice++;
+  }
